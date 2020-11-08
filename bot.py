1bfbe6b1f34fed15aaffc830c06f16ebbcae7a1a
TWOX OFFICAL <57103492+TWOX-dev@users.noreply.github.com>
2020-11-08 19:54:18 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord 
+from discord.ext import commands
+from time import sleep 
+import random,string
+token = ''
+bot = commands.Bot(command_prefix='Nitro.')
+@bot.event
+async def on_ready():
+	print('<Bot working>')
+@bot.command()
+async def generate(ctx):
+	while True:
+		await ctx.send("https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=24)))
+		sleep(1)
+bot.run(token)
