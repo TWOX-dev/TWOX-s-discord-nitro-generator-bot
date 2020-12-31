8a682b89af16652c499dcc08e870d88a2dc6a935
TWOX OFFICAL <57103492+TWOX-dev@users.noreply.github.com>
2020-12-31 19:02:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from time import sleep 
 import random
 import string
-token = '' #сюда нужен токен вашего бота
+token = '' #your bot's token/сюда нужен токен вашего бота
 bot = commands.Bot(command_prefix='Nitro.')
 @bot.event
 async def on_ready():
